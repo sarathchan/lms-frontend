bd733d5558fd2b324073fd08863e7742004db9fb
sarathchan <sarathchan20@gmail.com>
2026-04-11 03:24:48 -0700

    changes

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,4 +2,4 @@
  * Used in production only when `VITE_API_ORIGIN` is empty at build time.
  * If `.env.production` sets `VITE_API_ORIGIN`, that value wins (see `getApiOrigin`).
  */
-export const PRODUCTION_API_ORIGIN = 'http://13.201.48.42:3000'
+export const PRODUCTION_API_ORIGIN = 'https://complete-blair-trustee-atlantic.trycloudflare.com/docs'
